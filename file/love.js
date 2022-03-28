0a4ff1f05a7a5ba88f2de230e252719d75ed03e9
yanlincheng <81892151+yanlincheng@users.noreply.github.com>
2022-03-28 20:09:33 +0800

    Add files via upload

--- file/love.js
+++ file/love.js
@@ -170,7 +170,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("点我，就说明爱我哟", 23, 10);
-            ctx.restore();come
+            ctx.restore();
         },
         clear: function() {
             var ctx = this.tree.ctx, cirle = this.cirle;
